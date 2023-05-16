d4cf6caf563e9fb67d0ebc1cadf701e4dcfd8308
Aung Ko Man <aungkoman@gmail.com>
2023-05-16 12:41:10 +0630

    size models added

--- mm_learning_platform/elearning/models.py
+++ mm_learning_platform/elearning/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Instructor(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    dob = models.DateField()
+    profile_picture = models.FileField()
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    cover_photo = models.FileField()
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=200)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    description = models.TextField()
+    episode = models.IntegerField()
+    thumbnail = models.FileField()
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    dob = models.DateField()
+    profile_picture = models.FileField()
+    education = models.CharField(max_length=200)
+
+class Enroll(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
